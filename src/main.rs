a30ad7a15bae9c8399813020c9e8487a688472d4
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-06-26 15:35:07 +0000

    Init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate minigrep;
+use minigrep::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let config = Config::new(std::env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
